c4b84734fa69aaa129fd3e689b057fc6be2397e7
Simon Bonnedahl <simbo803@student.liu.se>
2023-01-17 15:46:53 +0100

    Grabbed linked list from gitlab

---
+++ pintos-linked-list-master/main.c
@@ -0,0 +1,70 @@
+#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct student {
+	char *name;
+	struct list_elem elem;
+};
+
+
+void insert (struct list *student_list) {
+}
+
+void delete (struct list *student_list) {
+}
+
+void list (struct list *student_list) {
+}
+
+void quit (struct list *student_list) {
+}
+
+int main() {
+	struct list student_list;
+	list_init (&student_list);
+	int opt;
+
+	do {
+		printf("Menu:\n");
+		printf("1 - Insert student\n");
+		printf("2 - Delete student\n");
+		printf("3 - List students\n");
+		printf("4 - Exit\n");
+		scanf("%d", &opt);
+		switch (opt) {
+			case 1:
+				{
+					insert(&student_list);
+					break;
+				}
+			case 2:
+				{
+					delete(&student_list);
+					break;
+				}
+			case 3:
+				{
+					list(&student_list);
+					break;
+				}
+
+			case 4:
+				{
+					quit(&student_list);
+					break;
+				}
+			default:
+				{
+					printf("Quit? (1/0):\n");
+					scanf("%d", &opt);
+					if (opt)
+						quit(&student_list);
+					break;
+				}
+		}
+	} while(1);
+		
+	return 0;
+}
