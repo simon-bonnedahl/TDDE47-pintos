841463d63128bdfdacd71312651a28720b04d553
Simon Bonnedahl <simbo803@student.liu.se>
2023-01-23 15:28:08 +0100

    init

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -4,17 +4,49 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
-static void syscall_handler (struct intr_frame *);
+static void syscall_handler(struct intr_frame *);
 
-void
-syscall_init (void) 
+void syscall_init(void)
 {
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler(struct intr_frame *f UNUSED)
 {
-  printf ("system call!\n");
-  thread_exit ();
+  // validate argument
+
+  // get system call number
+  int syscall_num = *(int *)f->esp;
+
+  switch (syscall_num)
+  {
+  case SYS_WRITE:
+    int fileDescriptor = *(int *)(f->esp + 4);
+    const void *buffer = *(const void **)(f->esp + 8);
+    unsigned size = *(unsigned *)(f->esp + 12);
+
+    if (fd < 0 || buffer == NULL || size < 0)
+    {
+      thread_exit();
+    }
+    void arguments[3] = {fileDescriptor, buffer, size};
+
+    thread_create("write", PRI_DEFAULT, write, arguments);
+
+    break;
+  case SYS_READ:
+    break;
+  case SYS_HALT:
+    break;
+  case SYS_EXIT:
+    break;
+  case SYS_EXEC:
+    break;
+
+  default:
+    thread_exit();
+    break;
+  }
+  printf("system call!\n");
 }
